e2e8d30cf871eb6c359025fccb85e15c2d2fe8a4
Baptiste <boruezabal@gmail.com>
2013-03-19 10:52:24 +0100

    Correction de la déterminisation (destructif à vérifier), début implémentation union (automate des couples)

--- startautomaton.py
+++ startautomaton.py
@@ -31,12 +31,13 @@ class startautomaton(automaton):
 
 	def get_epsilon_transitions(self):
 		liste = []
-		for e in self.get_states():					# Pour tous les etats
-			for eps in self.get_epsilons():			# Pour tous les caracteres representant epsilon
-				if (e, eps) in self._adjacence:		# Il nous faut une transition
+		for e in self.get_states():	# Pour tous les etats
+			for eps in self.get_epsilons():	# Pour tous les caracteres representant epsilon
+				if (e, eps) in self._adjacence:	# Il nous faut une transition
 					for dest in self._adjacence[(e, eps)]:
 						liste.append((e, eps, dest))
 		return liste
+
 
 	def est_deterministe(self):
 		res = True
@@ -49,52 +50,6 @@ class startautomaton(automaton):
 				break
 
 		return res
-
-	def lister_etats_indetermines(self, etat):
-		liste = []
-		for l in self.get_alphabet():					# pour chaque lettre de l'alphabet
-			tmp = self._delta(l, [etat])				
-			if (len(tmp) > 1):							# si plusieurs etats sont accessibles
-				for e in tmp:
-					liste.append(e)						# alors on les rajoute à la liste
-
-		return liste 									# on retourne la liste ainsi formee
-
-	# def copier_regroupement_etats(self, automate_origine, liste_etats):
-	# 	nouvel_etat = pretty_set(liste_etats)
-
-	# 	for e in self.get_states():
-	# 		for l in self.get_alphabet():
-	# 			if automate_origine._delta(l, [e]).symmetric_difference(nouvel_etat) == set():		# Si l'etat traite permet d'acceder a seulement les etats regroupes
-	# 				self.add_transition(e, l, nouvel_etat)											# alors on ajoute une transition de cet etat vers l'etat regroupe
-	# 				pass
-	# 			if e in automate_origine._delta(l, [nouvel_etat])
-
-
-	# def regrouper_etats(self, liste_etats):
-	# 	nouvel_etat = pretty_set(liste_etats)							# On nomme le nouvel etat
-	# 	for e in self.get_states():
-	# 		for a in self.get_alphabet():
-	# 			tmp = self._delta(a, [e])
-	# 			if e in liste_etats:									# Pour chacun des etats à regrouper
-	# 				for fin in tmp:						
-	# 					if not fin in liste_etats:								# On rajoute une transition partant l'etat a regrouper
-	# 						self.add_transition((nouvel_etat, a, fin))			# cas : on a (1, 'a', 2) donc ({0,1} , 'a', 2)
-	# 					else:
-	# 						self.add_transition((nouvel_etat, a, nouvel_etat))	# cas : on a (1, 'a' 0) donc ({0,1}, 'a', {0,1})
-	# 																			# cas 2 : on a (1, 'a', 1) donc ({0,1}, 'a', {0,1})
-
-	# 					self.remove_transition((e,a,fin))						# On efface l'ancienne transition
-	# 			elif not (nouvel_etat == pretty_set()) and  tmp.issuperset(nouvel_etat):					# si tous les etats regroupes sont accessibles
-	# 				print(nouvel_etat)
-	# 				print(self._delta(a, [e]))
-	# 				print(self._delta(a, [e]).issuperset(nouvel_etat))
-	# 				print("toto\n")
-	# 				self.add_transition( (e, a, nouvel_etat) )						# alors une transition de cet etat vers le nouvel etat
-	# 				for successeur in self._delta(a, [e]):
-	# 					self.remove_transition( (e, a, successeur) )					# et on efface les anciennes transitions
-		
-		# return nouvel_etat
 
 	def est_complet(self):		
 		res = True
@@ -162,12 +117,13 @@ class startautomaton(automaton):
 	def minimiser(self):
 		return self
 
-	def determinisation_mew(self):				# Fuck la police !
+	def determinisation(self, destructif=False):		
 		automate_tmp = startautomaton(
 		alphabet = self.get_alphabet(),
 		epsilons = self.get_epsilons())
 
 		self.remove_epsilon_transitions()
+		finaux = self.get_final_states()
 
 		automate_tmp.add_initial_state(pretty_set(self.get_initial_states()))
 
@@ -179,55 +135,31 @@ class startautomaton(automaton):
 			if not isinstance(etat_courant, pretty_set):
 				etat_courant = set(etat_courant)
 
-			for l in self.get_alphabet():
-				if not l in self.get_epsilons():
-					nouveau = self._delta(l, etat_courant)
-					if not nouveau in automate_tmp.get_states():
-						file_etats.append(nouveau)
-					automate_tmp.add_transition((etat_courant, l, nouveau))
+			if not etat_courant == set():
+				for l in self.get_alphabet():
+					if not l in self.get_epsilons():
+						nouveau = self._delta(l, etat_courant)
+						if not nouveau == set():
+							if not nouveau in automate_tmp.get_states():
+								file_etats.append(nouveau)
+							for e in nouveau:								
+								if e in finaux:
+									automate_tmp.add_final_state(nouveau)
+									break
 
+							automate_tmp.add_transition((etat_courant, l, nouveau))
 
+		if automate_tmp.get_final_states() == set():
+			print("ERREUR : l'automate n'a pas d'état final déterminisable")
+			return self
 
+		if destructif:
+			self.__init__(automate_tmp.get_alphabet(), automate_tmp.get_epsilons(),
+				automate_tmp.get_states(), automate_tmp.get_initial_states(),
+				automate_tmp.get_final_states(), automate_tmp.get_transitions())
 
-		
+		return automate_tmp
 
-
-
-	def determinisation(self):
-		self.remove_epsilon_transitions()
-		liste_initiaux = []
-		for e in self.get_initial_states():
-			liste_initiaux.append(e)
-
-		file_etats = Queue()
-		initial = self.regrouper_etats(liste_initiaux)
-		self.add_initial_state(initial)
-		file_etats.put(initial)
-		etats_finaux = self.get_final_states()
-		while not file_etats.empty():
-			etat_courant = file_etats.get()									# Je recupere letat a traiter
-			etats_indetermines = self.lister_etats_indetermines(etat_courant)	# Je recupere les liste des etats suivants a regrouper
-			final = False
-			for e in etats_indetermines :									# Je definis si letat obtenu de la fusion doit etre final
-				if e in etats_finaux:					
-					final = True
-					break
-			tmp = self.regrouper_etats(etats_indetermines)
-			if final:
-				self.add_final_state(tmp)
-
-			for l in self.get_alphabet():									# Je rajoute les etats suivants au traitement
-				for e in self._delta(l, [etat_courant]):
-					if e != etat_courant:	# puisque tu fais le delta, e sera forcement different de etat_courant, non ?
-						file_etats.put(e)
-
-
-
-
-		# if not self.est_complet():
-		# 	self.completer()
-		return self
-	
 	def miroir(self):
 		# Echange des etats finaux et initiaux
 		self.echanger_etats_ini_fin()
@@ -240,27 +172,38 @@ class startautomaton(automaton):
 			
 		return self
 	
-	def union(self, aut2, deterministe=True):
+	def union(self, aut2):
+
 		if self.get_alphabet() == aut2.get_alphabet():
 			# On travaille sur des automates deterministes
-			if deterministe and not aut2.est_deterministe():
+			if not aut2.est_deterministe():
 				aut2.determinisation()
+				aut2.renumber_the_states()
 
-			if deterministe and not self.est_deterministe():
+			if not self.est_deterministe():
 				self.determinisation()
+				self.renumber_the_states()
 			
 			# On travaille sur des automates complet
 			if not aut2.est_complet():
 				aut2.completer()
 
 			if not self.est_complet():
-				self.completer()
-			self.regrouper_etats([0,1,2])
+				self.completer()		
 
-			"""
-				création de l'automate des couples
-			"""
-			return self
+			automate_tmp = startautomaton(
+			alphabet = self.get_alphabet(),
+			epsilons = self.get_epsilons())
+
+			# Création états initiaux de l'automate de l'union
+
+
+			etat = pretty_set((self.get_initial_states(), aut2.get_initial_states()))
+			automate_tmp.add_initial_state(etat)
+
+			# Création de l'automate des couples (automate de l'union)
+			
+			return automate_tmp
 		else:
 			return None
 
@@ -303,16 +246,25 @@ if __name__ == "__main__":
 		alphabet,
 		epsilons,
 		states = [], initials = [0,1], finals = [3,4],
-		transitions=[(0,'a', 1), (1, 'b', 2), (2, 'b', 2), (2, 'b', 3), (2, 'a', 5), (3, 'a', 1), (3, 'a', 4), (4, 'b', 3), (5, 'b', 4), (5, 'b', 1)]
+		transitions=[(0,'a',1), (1,'b',2), (2,'b',2), (2,'b',3), (3,'a',4), (4, 'a', 5), (4, 'a', 1)]
+	)
+
+	b = startautomaton(
+		alphabet,
+		epsilons,
+		states = [], initials = [0], finals = [4],
+		transitions=[(0,'a',1), (1,'b',2), (2,'b',2), (3,'b',2), (3,'b',4), (2, 'a', 5), (4, 'a', 2)]
 	)
 
 """
 	Si problème avec l'affichage des labels
 	=> http://superuser.com/questions/334625/dotty-shows-all-labels-as-dots-period-instead-of-text
 """
-a.display("Avant", False)
-a.determinisation()
-a.display("Apres", False)
+a.display("A", False)
+b.display("B", False)
+b.determinisation(False).display("B deterministe")
+
+#a.union(b).display("Apres")
 
 """
 (0 'a' {1,2}) (5 'a' 2) ==> (0 'a' {1,2}) (5 'a' {1,2}) ?
